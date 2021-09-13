b164935d19686cbc1b61380ef5cdfa6bc3b173c0
iyarsius <iyarsius@gmail.com>
2021-09-13 06:10:29 +0200

    basic-version

---
+++ src/functions/configRequest.js
@@ -0,0 +1,25 @@
+const UserAgent = require('user-agents');
+
+module.exports = (proxy) => {
+    const userAgent = new UserAgent();
+
+    const axiosConfig = {
+        headers: {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+            "cache-control": "no-cache",
+            "pragma": "no-cache",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "none",
+            "sec-fetch-user": "?1",
+            "sec-gpc": "1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": userAgent.toString()
+        },
+    }
+
+    if (proxy) axiosConfig.proxy = proxy;
+
+    return axiosConfig
+}
---
+++ src/index.js
@@ -0,0 +1,116 @@
+const axios = require('axios');
+const stockxConfig = require('./functions/configStockx');
+const configRequest = require('./functions/configRequest');
+
+module.exports = {
+    /**
+     * Returns the first item found on stockx
+     * 
+     * @param {String} item Name or SKU to scrape
+     * @param {Object} options Optionnal settings
+     * @param {String} options.currency Currency ticker
+     * @param {String} options.country Country ticker
+     * @param {String} options.proxy Proxy url
+     */
+    getProduct: async (item, options) => {
+        if (typeof item !== "string") throw TypeError('Wrong item, please use string');
+        let baseURI = `https://stockx.com/api/browse?&_search=${item}`;
+
+        if (options?.currency !== undefined && !stockxConfig.currencys.includes(options?.currency)) throw SyntaxError(`${options.currency} is not a valid currency`);
+
+        const currency = options?.currency ? options.currency : "USD";
+        let extendURI = `&currency=${currency}`;
+
+        if (options?.country !== undefined && !stockxConfig.countrys.includes(options?.country)) throw SyntaxError(`${options.country} is not a valid country`);
+
+        const country = stockxConfig.countrys.includes(options?.country) ? options.country : "US";
+        extendURI = extendURI + `&country=${country}`;
+
+        const axiosOptions = configRequest(options?.proxy);
+
+        try {
+        const product = await axios.get(baseURI, axiosOptions).then(res => {
+            const items = res.data
+            const item = items.Products["0"]
+            if (!item) throw Error('No product found')
+            return {
+                name: item.title,
+                description: item.description.split("<br>").join("").split("\n\n").join("\n"),
+                image: item.media.imageUrl,
+                url: `https://stockx.com/${item.urlKey}`,
+                uuid: item.market.productUuid,
+                lastSale: item.market.lastSale,
+                "72hvolume": item.market["salesLast72Hours"],
+                retail: item.retailPrice,
+                sku: item.styleId,
+                colorway: item.colorway.toUpperCase(),
+                releaseDate: item.releaseDate,
+                seller: item.brand[0].toUpperCase() + item.brand.slice(1)
+            }
+        });
+
+        baseURI = `https://stockx.com/api/products/${product.uuid}?includes=market`
+        product.sizes = await axios.get(baseURI + extendURI, axiosOptions).then(res => {
+            const data = res.data;
+            const variants = data.Product.children
+
+            const sizes = [];
+            const convert = stockxConfig.sizes
+
+            for (const key in variants) {
+                const shoe = variants[key]
+                const sizeData = shoe.shoeSize;
+
+                const usSize = sizeData.replace(/[A-Z]/g, "")
+                let sizeConverter = convert.men.find(s => s.us === usSize)
+
+                if (!sizeConverter) continue;
+
+                let euSize = sizeConverter.eu
+                let sizeType = ""
+
+                if (sizeData.includes("W")) {
+                    sizeConverter = convert.women.find(s => s.us === usSize)
+                    if (!sizeConverter) continue;
+                    euSize = sizeConverter.eu
+                    sizeType = "W"
+                }
+
+                if (sizeData.includes("Y")) {
+                    sizeConverter = convert.gs.find(s => s.us === usSize)
+                    if (!sizeConverter) continue;
+                    euSize = sizeConverter.eu
+                    sizeType = "Y"
+                }
+
+                if (shoe.title.toLowerCase().includes("adidas")) {
+                    sizeConverter = convert.adidas.find(s => s.us === usSize)
+                    if (!sizeConverter) continue;
+                    euSize = sizeConverter.eu
+                    sizeType = ""
+                }
+
+                if (sizeData.includes("K")) {
+                    euSize = "--"
+                    sizeType = "K"
+                }
+
+
+                sizes.push({
+                    sizeUS: usSize,
+                    sizeEU: euSize,
+                    sizeType: sizeType,
+                    lowestAsk: shoe.market.lowestAsk,
+                    highestBid: shoe.market.highestBid,
+                    lastSale: shoe.market.lastSale
+                })
+            }
+            return sizes
+        })
+        return product
+    } catch (e) {
+        if (e.code === "ECONNREFUSED") throw Error('Connection not possible')
+        throw Error(e.message)
+    }
+    }
+}
